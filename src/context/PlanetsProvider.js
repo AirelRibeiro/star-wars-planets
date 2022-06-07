12410bc1d23fd5f39a2fbee02b54fd6408dae6e0
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-07 20:24:11 -0300

    feat: State para SelectsFilter com hook useState

--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -1,19 +1,31 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import PlanetsContext from './PlanetsContext';
-// import response from '../testData';
+import response from '../testData';
 
 const PlanetsProvider = ({ children }) => {
   const [data, setData] = useState([]);
   const [filterByName, setFilterByName] = useState('');
+  const [selectFilter, setSelectFilter] = useState([{
+    column: 'population',
+    comparison: 'maior que',
+    value: '100000',
+  }]);
 
   const fetchPlanetsInformation = async () => {
     const planetsResponse = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
     const planetsData = await planetsResponse.json();
-    setData(planetsData.results);
+    setData(response.results);
   };
 
-  const context = { data, fetchPlanetsInformation, filterByName, setFilterByName };
+  const context = {
+    data,
+    fetchPlanetsInformation,
+    filterByName,
+    setFilterByName,
+    selectFilter,
+    setSelectFilter };
+
   return (
     <PlanetsContext.Provider value={ context }>
       {children}
