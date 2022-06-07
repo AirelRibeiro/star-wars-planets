eed897d9d149d24c2751039347e828c51be0b25f
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-07 17:50:25 -0300

    feat: Alteração para que o input seja controlado pelo State filterByName presente no PlanetsContext

--- src/components/FilterInput.js
+++ src/components/FilterInput.js
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import PlanetsContext from '../context/PlanetsContext';
 
-const FilterInput = () => (<input type="text" />);
+const FilterInput = () => {
+  const { setFilterByName, filterByName } = useContext(PlanetsContext);
+  const handleName = ({ target }) => setFilterByName(target.value);
+  return (
+    <input
+      type="text"
+      data-testid="name-filter"
+      value={ filterByName }
+      onChange={ handleName }
+    />
+  );
+};
 
 export default FilterInput;
