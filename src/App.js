c4c9db4794f6c3f1215ef02db813b11336a1a605
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-07 17:06:08 -0300

    feat: Inclusão de input no App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PlanetsProvider from './context/PlanetsProvider';
 import Table from './components/Table';
+import FilterInput from './components/FilterInput';
 import './App.css';
 
 function App() {
   return (
     <PlanetsProvider>
       <Table>Hello, App!</Table>
+      <FilterInput />
     </PlanetsProvider>
   );
 }
