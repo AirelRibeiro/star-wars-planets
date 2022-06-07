cff0af1bc833964cfe937d6bf92a1310a5d82d41
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-07 20:25:47 -0300

    feat: Inclusão do State do PlanetsContext usando hook useContext

--- src/components/SelectsFilter.js
+++ src/components/SelectsFilter.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import PlanetsContext from '../context/PlanetsContext';
 
-const SelectsFilter = () => (
-  <div>
-    <select name="column">
-      <option value="population" selected>population</option>
-      <option value="orbital_period">orbital_period</option>
-      <option value="diameter">diameter</option>
-      <option value="rotation_period">rotation_period</option>
-      <option value="surface_water">surface_water</option>
-    </select>
-    <select name="comparison">
-      <option value="menor que">menor que</option>
-      <option value="maior que" selected>maior que</option>
-      <option value="igual a">igual a</option>
-    </select>
-    <input type="number" name="value" />
-  </div>);
+const SelectsFilter = () => {
+  const { selectFilter, setSelectFilter } = useContext(PlanetsContext);
+  return (
+    <div>
+      <select name="column" value={ selectFilter.column }>
+        <option value="population" selected>population</option>
+        <option value="orbital_period">orbital_period</option>
+        <option value="diameter">diameter</option>
+        <option value="rotation_period">rotation_period</option>
+        <option value="surface_water">surface_water</option>
+      </select>
+      <select name="comparison" value={ selectFilter.comparison }>
+        <option value="menor que">menor que</option>
+        <option value="maior que" selected>maior que</option>
+        <option value="igual a">igual a</option>
+      </select>
+      <input type="number" name="value" value={ selectFilter.value } />
+      <button type="button">Filtrar</button>
+    </div>
+  );
+};
 
 export default SelectsFilter;
