6ccdbbf7d342cb003fcb3626b89f8b0aee944ac3
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-07 20:51:35 -0300

    fix: In~clusão de thead e tbody

--- src/components/Table.js
+++ src/components/Table.js
@@ -9,44 +9,46 @@ const Table = () => {
 
   return (
     <table>
-      <tr>
-        <th>Name</th>
-        <th>Rotation Period</th>
-        <th>Orbital Period</th>
-        <th>Diameter</th>
-        <th>Climate</th>
-        <th>Gravity</th>
-        <th>Terrain</th>
-        <th>Surface Water</th>
-        <th>Population</th>
-        <th>Films</th>
-        <th>Created</th>
-        <th>Edited</th>
-        <th>URL</th>
-      </tr>
-
-      {data.filter(({ name }) => name.includes(filterByName)).map((planet) => (
-        <tr key={ planet.name }>
-          <td>{ planet.name }</td>
-          <td>{ planet.rotation_period }</td>
-          <td>{ planet.orbital_period }</td>
-          <td>{ planet.diameter}</td>
-          <td>{ planet.climate }</td>
-          <td>{ planet.gravity}</td>
-          <td>{ planet.terrain }</td>
-          <td>{ planet.surface_water }</td>
-          <td>{ planet.population }</td>
-          <td>
-            { planet.films.map((film) => (
-              <p key={ film }>film</p>
-            )) }
-          </td>
-          <td>{ planet.created }</td>
-          <td>{ planet.edited}</td>
-          <td>{ planet.url }</td>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Rotation Period</th>
+          <th>Orbital Period</th>
+          <th>Diameter</th>
+          <th>Climate</th>
+          <th>Gravity</th>
+          <th>Terrain</th>
+          <th>Surface Water</th>
+          <th>Population</th>
+          <th>Films</th>
+          <th>Created</th>
+          <th>Edited</th>
+          <th>URL</th>
         </tr>
-      ))}
-
+      </thead>
+      <tbody>
+        {data.filter(({ name }) => name.includes(filterByName)).map((planet) => (
+          <tr key={ planet.name }>
+            <td>{ planet.name }</td>
+            <td>{ planet.rotation_period }</td>
+            <td>{ planet.orbital_period }</td>
+            <td>{ planet.diameter}</td>
+            <td>{ planet.climate }</td>
+            <td>{ planet.gravity}</td>
+            <td>{ planet.terrain }</td>
+            <td>{ planet.surface_water }</td>
+            <td>{ planet.population }</td>
+            <td>
+              { planet.films.map((film) => (
+                <p key={ film }>film</p>
+              )) }
+            </td>
+            <td>{ planet.created }</td>
+            <td>{ planet.edited}</td>
+            <td>{ planet.url }</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 };
