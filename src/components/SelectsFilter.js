bba182c37c4ba85895eed0e3df344d533801902f
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-07 20:52:03 -0300

    fix: Nomeação característica de State

--- src/components/SelectsFilter.js
+++ src/components/SelectsFilter.js
@@ -2,22 +2,41 @@ import React, { useContext } from 'react';
 import PlanetsContext from '../context/PlanetsContext';
 
 const SelectsFilter = () => {
-  const { selectFilter, setSelectFilter } = useContext(PlanetsContext);
+  const { selectNumberFilter, setSelectNumberFilter } = useContext(PlanetsContext);
+
+  const handleSelectChange = ({ target }) => {
+    const newSelectFilter = [{ ...selectNumberFilter[0], [target.name]: target.value }];
+    setSelectNumberFilter(newSelectFilter);
+  };
+
   return (
     <div>
-      <select name="column" value={ selectFilter.column }>
+      <select
+        name="column"
+        value={ selectNumberFilter.column }
+        onChange={ handleSelectChange }
+      >
         <option value="population" selected>population</option>
         <option value="orbital_period">orbital_period</option>
         <option value="diameter">diameter</option>
         <option value="rotation_period">rotation_period</option>
         <option value="surface_water">surface_water</option>
       </select>
-      <select name="comparison" value={ selectFilter.comparison }>
+      <select
+        name="comparison"
+        value={ selectNumberFilter.comparison }
+        onChange={ handleSelectChange }
+      >
         <option value="menor que">menor que</option>
         <option value="maior que" selected>maior que</option>
         <option value="igual a">igual a</option>
       </select>
-      <input type="number" name="value" value={ selectFilter.value } />
+      <input
+        type="number"
+        name="value"
+        value={ selectNumberFilter.value }
+        onChange={ handleSelectChange }
+      />
       <button type="button">Filtrar</button>
     </div>
   );
