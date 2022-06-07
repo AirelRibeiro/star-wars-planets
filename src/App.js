9c8e372fd111a957f0372afb6a581b5aa8297628
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-07 16:28:07 -0300

    feat: Renderização de componente Table em App com acesso ao PlanetsProvider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import PlanetsProvider from './context/PlanetsProvider';
+import Table from './components/Table';
 import './App.css';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <PlanetsProvider>
+      <Table>Hello, App!</Table>
+    </PlanetsProvider>
   );
 }
 
