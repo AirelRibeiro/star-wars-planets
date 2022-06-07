d553fe5c62e7321f2d86d9f8789bdb7abb001245
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-07 16:20:28 -0300

    feat: PlanetProvider:  uso do hook useState para criar state data e salvar o array de resultados dos planetas; função fetchPlanetsInformation para requisitar planetas na API e armazenar no state a partir de setData; disponibilização do context para filhos do Provider

---
+++ src/context/PlanetsProvider.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import PlanetsContext from './PlanetsContext';
+
+const PlanetsProvider = ({ children }) => {
+  const [data, setData] = useState([]);
+
+  const fetchPlanetsInformation = async () => {
+    const planetsResponse = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
+    const planetsData = await planetsResponse.json();
+    setData(planetsData.results);
+  };
+
+  const context = { data, fetchPlanetsInformation };
+  return (
+    <PlanetsContext.Provider value={ context }>
+      {children}
+    </PlanetsContext.Provider>
+  );
+};
+
+PlanetsProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default PlanetsProvider;
