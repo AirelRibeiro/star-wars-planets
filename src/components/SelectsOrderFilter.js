cbc943302ce24a65b2975b4540c6db2bc429b91c
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-08 19:55:43 -0300

    feat: Sort button

--- src/components/SelectsOrderFilter.js
+++ src/components/SelectsOrderFilter.js
@@ -4,30 +4,39 @@ const SelectsOrderFilter = () => {
   const [columnFilter, setColumnFilter] = useState([
     'population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water']);
   return (
-    <select
-      name="column"
-      data-testid="column-sort"
-      value={  }
-      onChange={  }
-    >
-      {columnFilter
-        .map((option) => (<option key={ option } value={ option }>{ option }</option>))}
-    </select>
-    <fieldset>
-    <legend>Select order</legend>
-
-
-      <input type="radio" id="huey" name="drone" value="huey"
-             checked>
-      <label for="huey">Huey</label>
-
-
-      <input type="radio" id="dewey" name="drone" value="dewey">
-      <label for="dewey">Dewey</label>
-  
-      <input type="radio" id="louie" name="drone" value="louie">
-      <label for="louie">Louie</label>
-</fieldset>
+    <div>
+      <select
+        name="column"
+        data-testid="column-sort"
+        value={  }
+        onChange={  }
+      >
+        {columnFilter
+          .map((option) => (<option key={ option } value={ option }>{ option }</option>))}
+      </select>
+      <fieldset>
+        <legend>Select order</legend>
+          <input
+          type="radio"
+          data-testid="column-sort-input-asc"
+          name="drone"
+          value="ASC" />
+          <label for="huey">Huey</label>
+          <input
+          type="radio"
+          data-testid="column-sort-input-desc"
+          name="drone"
+          value="DESC" />
+          <label for="huey">Huey</label>
+      </fieldset>
+      <button
+        type="button"
+        data-testid="column-sort-button"
+        onClick={}
+      >
+        Ordenar
+      </button>
+    </div>
   );
 };
 
