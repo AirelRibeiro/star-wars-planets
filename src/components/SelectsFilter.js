e36afe24ed3a9a641a1998b58a7d02eda646824e
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-07 23:08:03 -0300

    feat: State para guardar a existência de filtros

--- src/components/SelectsFilter.js
+++ src/components/SelectsFilter.js
@@ -10,7 +10,8 @@ const SelectsFilter = () => {
     selectFiltersAvailables,
     setSelectFiltersAvailables } = useContext(PlanetsContext);
 
-  const [usedFilters, setUsedFilters] = useState();
+  const [usedFilters, setUsedFilters] = useState([]);
+  const [isFiltered, setIsFiltered] = useState(false);
 
   const handleSelectChange = ({ target }) => {
     const newSelectFilter = [{ ...selectNumberFilter[0], [target.name]: target.value }];
@@ -22,24 +23,41 @@ const SelectsFilter = () => {
     setSelectFiltersAvailables(newOptions);
   };
 
+  const saveFilter = (filterType, comparsionType, number) => {
+    const filter = `${filterType} ${comparsionType} ${number}`;
+    setUsedFilters([...usedFilters, filter]);
+    setIsFiltered(true);
+  };
+
+  const deletFilter = (usedFilter) => {
+    const newArrayOfFilters = usedFilters.filter((filter) => filter !== usedFilter);
+    setUsedFilters(newArrayOfFilters);
+    if (newArrayOfFilters.length <= 0) {
+      setIsFiltered(false);
+    }
+  };
+
   const filteredPlanets = (filterType, comparsionType, number) => {
     if (comparsionType === 'maior que') {
       const newPlanetsData = dataForFilter
         .filter((planet) => planet[filterType] > Number(number));
       setDataForFilter(newPlanetsData);
       availablesFilters(filterType);
+      saveFilter(filterType, comparsionType, number);
     }
     if (comparsionType === 'menor que') {
       const newPlanetsData = dataForFilter
         .filter((planet) => planet[filterType] < Number(number));
       setDataForFilter(newPlanetsData);
       availablesFilters(filterType);
+      saveFilter(filterType, comparsionType, number);
     }
     if (comparsionType === 'igual a') {
       const newPlanetsData = dataForFilter
         .filter((planet) => planet[filterType] === number);
       setDataForFilter(newPlanetsData);
       availablesFilters(filterType);
+      saveFilter(filterType, comparsionType, number);
     }
   };
 
@@ -56,6 +74,12 @@ const SelectsFilter = () => {
 
   return (
     <div>
+      {isFiltered && usedFilters.map((usedFilter) => (
+        <p key={ usedFilter }>
+          {usedFilter}
+          <button type="button" onClick={ () => deletFilter(usedFilter) }>X</button>
+        </p>
+      ))}
       <select
         name="column"
         data-testid="column-filter"
