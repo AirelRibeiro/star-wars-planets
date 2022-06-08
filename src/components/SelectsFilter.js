467c090e80ec2f26fe4846f90b7b55db5692ab5b
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-07 22:06:47 -0300

    feat: Função filteredPlanets para filtrar os planetas por números relativos a population, orbital_period, diameter, rotation_period e surface_water; atualização do State do PlanetContext a partir de array filtrado

--- src/components/SelectsFilter.js
+++ src/components/SelectsFilter.js
@@ -2,21 +2,55 @@ import React, { useContext } from 'react';
 import PlanetsContext from '../context/PlanetsContext';
 
 const SelectsFilter = () => {
-  const { selectNumberFilter, setSelectNumberFilter } = useContext(PlanetsContext);
+  const {
+    selectNumberFilter,
+    setSelectNumberFilter,
+    dataForFilter,
+    setDataForFilter } = useContext(PlanetsContext);
 
   const handleSelectChange = ({ target }) => {
     const newSelectFilter = [{ ...selectNumberFilter[0], [target.name]: target.value }];
     setSelectNumberFilter(newSelectFilter);
   };
 
+  const filteredPlanets = (filterType, comparsionType, number) => {
+    if (comparsionType === 'maior que') {
+      const newPlanetsData = dataForFilter
+        .filter((planet) => planet[filterType] > Number(number));
+      setDataForFilter(newPlanetsData);
+    }
+    if (comparsionType === 'menor que') {
+      const newPlanetsData = dataForFilter
+        .filter((planet) => planet[filterType] < Number(number));
+      setDataForFilter(newPlanetsData);
+    }
+    if (comparsionType === 'igual a') {
+      const newPlanetsData = dataForFilter
+        .filter((planet) => planet[filterType] === number);
+      setDataForFilter(newPlanetsData);
+    }
+  };
+
+  // const selectedFilters = (filterType, comparsionType, number) => {
+  //   switch (filterType) {
+  //   case 'population':
+
+  //     break;
+
+  //   default:
+  //     break;
+  //   }
+  // };
+
   return (
     <div>
       <select
         name="column"
-        value={ selectNumberFilter.column }
+        data-testid="column-filter"
+        value={ selectNumberFilter[0].column }
         onChange={ handleSelectChange }
       >
-        <option value="population" selected>population</option>
+        <option value="population">population</option>
         <option value="orbital_period">orbital_period</option>
         <option value="diameter">diameter</option>
         <option value="rotation_period">rotation_period</option>
@@ -24,20 +58,33 @@ const SelectsFilter = () => {
       </select>
       <select
         name="comparison"
-        value={ selectNumberFilter.comparison }
+        data-testid="comparison-filter"
+        value={ selectNumberFilter[0].comparison }
         onChange={ handleSelectChange }
       >
+        <option value="maior que">maior que</option>
         <option value="menor que">menor que</option>
-        <option value="maior que" selected>maior que</option>
         <option value="igual a">igual a</option>
       </select>
       <input
         type="number"
         name="value"
-        value={ selectNumberFilter.value }
+        data-testid="value-filter"
+        value={ selectNumberFilter[0].value }
         onChange={ handleSelectChange }
       />
-      <button type="button">Filtrar</button>
+      <button
+        type="button"
+        data-testid="button-filter"
+        onClick={
+          () => (
+            filteredPlanets(selectNumberFilter[0].column,
+              selectNumberFilter[0].comparison, selectNumberFilter[0].value))
+        }
+
+      >
+        Filtrar
+      </button>
     </div>
   );
 };
