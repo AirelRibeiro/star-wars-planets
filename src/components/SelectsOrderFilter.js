ec76007d266978061d6581bd91ecc5e5d4eb7781
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-06-08 21:06:51 -0300

    fix: Estrutura da disposição do código

--- src/components/SelectsOrderFilter.js
+++ src/components/SelectsOrderFilter.js
@@ -1,21 +1,35 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import PlanetsContext from '../context/PlanetsContext';
 
 const SelectsOrderFilter = () => {
-  const [columnFilter, setColumnFilter] = useState([
+  const [columnFilter] = useState([
     'population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water']);
+  const { selectOrderFilter, setSelectOrderFilter } = useContext(PlanetsContext);
+
+  const handleSelectChange = ({ target }) => {
+    const newSelectFilter = [{ ...selectOrderFilter[0], [target.name]: target.value }];
+    setSelectOrderFilter(newSelectFilter);
+  };
+
   return (
     <div>
-      <select
-        name="column"
-        data-testid="column-sort"
-        // value={  }
-        // onChange={  }
-      >
-        {columnFilter
-          .map((option) => (<option key={ option } value={ option }>{ option }</option>))}
-      </select>
       <fieldset>
         <legend>Select order</legend>
+        <select
+          name="column"
+          data-testid="column-sort"
+          value={ selectOrderFilter[0].column }
+          onChange={ handleSelectChange }
+        >
+          {columnFilter
+            .map((option) => (
+              <option
+                key={ option }
+                value={ option }
+              >
+                { option }
+              </option>))}
+        </select>
         <label htmlFor="ASC">
           <input
             type="radio"
@@ -36,14 +50,13 @@ const SelectsOrderFilter = () => {
           />
           Descendente
         </label>
+        <button
+          type="button"
+          data-testid="column-sort-button"
+        >
+          Ordenar
+        </button>
       </fieldset>
-      <button
-        type="button"
-        data-testid="column-sort-button"
-        // onClick={}
-      >
-        Ordenar
-      </button>
     </div>
   );
 };
