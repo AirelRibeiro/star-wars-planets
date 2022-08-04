2a715bbdca154dddd0cbcfc7b340b72148a6ef8a
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-03 22:43:07 -0300

    style: estiliza filtro de nome

--- src/components/FilterInput.js
+++ src/components/FilterInput.js
@@ -1,15 +1,19 @@
 import React, { useContext } from 'react';
 import PlanetsContext from '../context/PlanetsContext';
+import '../style/FilterInput.css';
 
 const FilterInput = () => {
   const { setFilterByName, filterByName } = useContext(PlanetsContext);
   const handleName = ({ target }) => setFilterByName(target.value);
   return (
-    <input
-      type="text"
-      value={ filterByName }
-      onChange={ handleName }
-    />
+    <div className="filter-by-name-div">
+      <input
+        type="text"
+        value={ filterByName }
+        onChange={ handleName }
+        placeholder="Busque seu planeta pelo nome"
+      />
+    </div>
   );
 };
 
