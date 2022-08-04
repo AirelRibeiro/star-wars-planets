0866ce801face3f97114337a3400f7042e8cb16a
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-04 20:54:12 -0300

    refactor: muda API para a oficial de Star Wars

--- src/API/fetchApi.js
+++ src/API/fetchApi.js
@@ -1,6 +1,6 @@
 const fetchPlanetsInformation = async (setData, setDataForFilter) => {
   const negativeOne = -1;
-  const planetsResponse = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
+  const planetsResponse = await fetch('https://swapi.dev/api/planets/');
   const planetsData = await planetsResponse.json();
   setData(planetsData.results);
   setDataForFilter(planetsData.results
